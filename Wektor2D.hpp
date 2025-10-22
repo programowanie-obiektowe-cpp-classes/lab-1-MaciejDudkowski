ba3541cff461686202f1b41a8050a35b649b8662
Maciej Dudkowski <01170746@pw.edu.pl>
2025-10-22 08:43:11 +0200

    ZAliczenie lab 1

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,25 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+
+private:
+    double x;
+    double y;
+
+public:
+    Wektor2D() = default;
+
+    Wektor2D(double a, double b) : x(a), y(b)
+    {
+        cout << "Konstruktor dwuargumentowy: (" << x << ", " << y << ")" << endl;
+    }
+
+    void setX(double a) { x = a; }
+    void setY(double b) { y = b; }
+
+    double getX() const { return x; }
+    double getY() const { return y; }
+
+    Wektor2D operator+(const Wektor2D& other) const { return Wektor2D(x + other.x, y + other.y); }
+
+    double operator*(const Wektor2D& other) const { return x * other.x + y * other.y; }
 };
